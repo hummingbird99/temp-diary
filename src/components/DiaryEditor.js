d906592db44d431f2cc8b451bdbaaac1543cf068
hummingbird99 <sseoulite@gmail.com>
2023-03-09 16:19:41 +0900

    Init: 다이어리 에디터 기본 틀 구현

---
+++ src/components/DiaryEditor.js
@@ -0,0 +1,73 @@
+import React, { useRef, useState } from "react";
+import styles from "./DiaryEditor.module.css";
+
+const DiaryEditor = () => {
+  const authorInput = useRef(); // React.mutableRefObject: HTML DOM 요소에 접근
+  const contentInput = useRef();
+
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handlerChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handlerSubmit = () => {
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
+
+    alert("저장 성공");
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          ref={authorInput}
+          name="author"
+          value={state.author}
+          onChange={handlerChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          ref={contentInput}
+          name="content"
+          value={state.content}
+          onChange={handlerChangeState}
+        />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handlerChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handlerSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
