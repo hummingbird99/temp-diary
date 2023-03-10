97d5a97a268669e84000b6f38f31ff2d7fa348e0
hummingbird99 <sseoulite@gmail.com>
2023-03-10 15:17:52 +0900

    Feat: React.memo 연습 구현

--- src/components/OptimizeTest.js
+++ src/components/OptimizeTest.js
@@ -8,13 +8,22 @@ const CounterA = React.memo(({ count }) => {
   return <div>{count}</div>;
 });
 
-const CounterB = React.memo(({ obj }) => {
+const CounterB = ({ obj }) => {
+  // 얕은 비교를 하지 않도록 렌더링 최적화
   useEffect(() => {
     console.log(`CounterB Update - count : ${obj.count}`);
   });
 
   return <div>{obj.count}</div>;
-});
+};
+
+const areEqual = (prevProps, nextProps) => {
+  // return true = 이전 props 현재 props가 같다 -> 리렌더링을 일으키지 않는다.
+  // return false = 이전 과 현재가 다르다 -> 리렌더링을 일으킨다.
+  return prevProps.obj.count === nextProps.obj.count;
+};
+
+const MemoizedCounterB = React.memo(CounterB, areEqual); // 렌더링 되지 않는다.
 
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
@@ -31,7 +40,7 @@ const OptimizeTest = () => {
       </div>
       <div>
         <h2>Counter B</h2>
-        <CounterB obj={obj} />
+        <MemoizedCounterB obj={obj} />
         <button
           onClick={() =>
             setObj({
